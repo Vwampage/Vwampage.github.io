b9d7b60a22ec949664413043a637d36081e5c871
Vwampage <vwampage@gmail.com>
2013-12-06 12:25:07 -0600

    added better dragging to Summon and changed image names in portfolio to actually work. lol

--- Summon/js/dragonDrop.js
+++ Summon/js/dragonDrop.js
@@ -1,6 +1,24 @@
 $('.draggable').draggable();
-$('.droppable').droppable();
-$('.droppable').on("drop", function(event, ui){
+$('.include').droppable();
+$('.exclude').droppable();
+$('.include').on("drop", function(event, ui){
 		$(this).css("background-color", "pink");
-		console.log("dropped");
+		var newimg = $("<img>");
+		ui.draggable.hide();
+		newimg.attr("src", ui.draggable.context.src);
+		console.log(ui.draggable.context.src);
+		var newarticle = $("<article>").append(newimg);
+		var newli = $("<li>").append(newarticle);
+
+		//newimg =newimg.wrap("<article></article>");
+		//newimg =newimg.wrap("<li></li>");
+		$('.dropzone').append(newli);
+
+
+	
 	});
+//$('.dropzone').add('li')
+//var person = 
+
+
+
