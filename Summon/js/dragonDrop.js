d222233b7e0f474c23da3a922e82f4bd6b6e4133
Vwampage <vwampage@gmail.com>
2013-12-10 12:36:21 -0600

    Changed to jquery mobile and tweaked copy on portfolio site

--- Summon/js/dragonDrop.js
+++ Summon/js/dragonDrop.js
@@ -12,13 +12,25 @@ $('.include').on("drop", function(event, ui){
 
 		//newimg =newimg.wrap("<article></article>");
 		//newimg =newimg.wrap("<li></li>");
+		$('.dropzone').append(newli);	
+	});
+//$('.dropzone').add('li')
+//var person = 
+
+$('.exclude').on("drop", function(event, ui){
+		$(this).css("background-color", "pink");
+		var newimg = $("<img>");
+		ui.draggable.hide();
+		newimg.attr("src", ui.draggable.context.src);
+		console.log(ui.draggable.context.src);
+		var newarticle = $("<article>").append(newimg);
+		var newli = $("<li>").append(newarticle);
+
+		//newimg =newimg.wrap("<article></article>");
+		//newimg =newimg.wrap("<li></li>");
 		$('.dropzone').append(newli);
 
 
 	
 	});
-//$('.dropzone').add('li')
-//var person = 
-
-
 
---
+++ Summon/top3/js/dragonDrop.js
@@ -0,0 +1,23 @@
+$('.draggable').draggable();
+$('.include').droppable();
+$('.exclude').droppable();
+$('.include').on("drop", function(event, ui){
+		var newimg = $("<img>");
+		ui.draggable.hide();
+		newimg.attr("src", ui.draggable.context.src);
+		console.log(ui.draggable.context.src);
+		var newarticle = $("<article>").append(newimg);
+		var newli = $("<li>").append(newarticle);
+
+		//newimg =newimg.wrap("<article></article>");
+		//newimg =newimg.wrap("<li></li>");
+		$('.dropzone').append(newli);
+
+
+	
+	});
+//$('.dropzone').add('li')
+//var person = 
+
+
+
